30505a5851b6c7351da379237a9542210361197c
Florian <Portikus@users.noreply.github.com>
2015-09-24 17:28:45 +0000

    Added Unity to the TestProject Added a BuildScript which delploys the library

---
+++ Universial.Test/UnityTestHelper/UnityTestBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Universial.Test.UnityTestHelper
+{
+    public class UnityTestBase<T> : TestBase<T>
+    {
+        protected UnityContainer Container;
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            Container = new UnityContainer();
+        }
+    }
+}
