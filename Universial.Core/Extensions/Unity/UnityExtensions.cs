56f6ca0c0cb4a15b08b14f05a591c7ded2fd02ba
Florian <Portikus@users.noreply.github.com>
2015-09-24 17:46:03 +0000

    Added Unity/UnityExtensions

---
+++ Universial.Core/Extensions/Unity/UnityExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Universial.Core.Extensions.Unity
+{
+    public static class UnityExtensions
+    {
+        /// <summary>
+        /// Registers a instance of the passed type <typeparamref name="T"/>. Uses a <see cref="ContainerControlledLifetimeManager"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="container"></param>
+        public static void RegisterInstance<T>(this UnityContainer container)
+        {
+            container.RegisterType<T>(container.Resolve<ContainerControlledLifetimeManager>());
+        }
+
+        /// <summary>
+        /// Registers a instance of the passed type <typeparamref name="TType"/> and mappes it to the type <typeparamref name="TBase"/>.
+        ///  Uses a <see cref="ContainerControlledLifetimeManager"/>
+        /// </summary>
+        /// <typeparam name="TType"></typeparam>
+        /// <typeparam name="TBase"></typeparam>
+        /// <param name="container"></param>
+        public static void RegisterInstance<TBase,TType>(this UnityContainer container) where TType : TBase
+        {
+            container.RegisterType<TBase,TType>(container.Resolve<ContainerControlledLifetimeManager>());
+        }
+    }
+}
---
+++ Universial.Tests.Core/Extensions/Unity/UnityExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Universial.Core.Extensions.Unity;
+using Universial.Test;
+
+namespace Universial.Tests.Core.Extensions.Unity
+{
+    public class UnityExtensionsTest : TestBase<UnityContainer>
+    {
+        [SetUp]
+        protected override void SetUp()
+        {
+            SystemUnderTest = new UnityContainer();
+            SystemUnderTest.RegisterInstance<UnityExtensionsTest>();
+            SystemUnderTest.RegisterInstance< TestBase ,UnityExtensionsTest >();
+        }
+
+        [Test]
+        public void Test_if_RegisterInstanceOfT_returns_always_the_same_object()
+        {
+            var expected = SystemUnderTest.Resolve<UnityExtensionsTest>();
+            Assert.That(()=>SystemUnderTest.Resolve<UnityExtensionsTest>(),Is.SameAs(expected));
+        }
+
+        [Test]
+        public void Test_if_RegisterInstanceOfTBaseOfTType_returns_always_the_same_object()
+        {
+            var expected = SystemUnderTest.Resolve<TestBase>();
+            Assert.That(() => SystemUnderTest.Resolve<TestBase>(), Is.SameAs(expected));
+        }
+    }
+}
